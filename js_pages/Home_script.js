bd4e3be402818e401cbfeda6843a57e8e7444255
plcrti <gumbatova_03@mail.ru>
2024-12-15 21:46:08 +0300

    Update Home_script.js

--- js_pages/Home_script.js
+++ js_pages/Home_script.js
@@ -1,11 +1,11 @@
 const button = document.getElementById('myButton');
 
 button.addEventListener('click', function() {
-window.location.href = '../Сайт Make-Up-Studio/pages/Skin_care_price.html'; 
+window.location.href = 'pages/Skin_care_price.html'; 
 });
 
 const button_1 = document.getElementById('myButton_1');
                 
 button_1.addEventListener('click', function() {
-window.location.href = '../Сайт Make-Up-Studio/pages/Make_up_price.html'; 
+window.location.href = 'pages/Make_up_price.html'; 
 });
